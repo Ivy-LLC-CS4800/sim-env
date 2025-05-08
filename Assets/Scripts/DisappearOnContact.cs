0bd7c021c27a6f5a9fcd95aff82ff2cd53e27a44
abbypin <68487724+abbypin@users.noreply.github.com>
2025-05-08 16:51:13 -0700

    Upload Changes

---
+++ Assets/Scripts/DisappearOnContact.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class DisappearOnContact : MonoBehaviour {
+
+    public GameObject targetObject; // The object that will disappear on contact
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject == targetObject)  {
+            gameObject.SetActive(false); // Deactivate the target object on contact
+            Debug.Log("Object has disappeared on contact with: " + collision.gameObject.name); // Log the name of the object that disappeared
+        }//end if       
+    }//end OnCollisionEnter()
+}//end DisappearOnContact
